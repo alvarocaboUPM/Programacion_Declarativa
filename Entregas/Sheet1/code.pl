71c2d21ff1be9a24633e7e9310f0c4b55c584aa7
alvarocabo <alvarocaboac2@gmail.com>
2023-02-28 20:35:41 +0100

    Entrega 1

--- Entregas/Sheet1/code.pl
+++ Entregas/Sheet1/code.pl
@@ -1,4 +1,4 @@
-:- module(E1, [elimina/2]).
+:- module(E1, [author_data/4, elimina/2, hermano/2, padre/2, hijo/2, abuela/2, tio/2, cunyado/2, madre/2]).
 author_data('Cabo', 'Ciudad', 'Alvaro', '200172').
 
 % EJERCICIO 1 %
@@ -24,3 +24,25 @@ garantiza_impunidad(_). % Se asume que todos pueden garantizar la impunidad.
 % ?- elimina(X, Y).
 
 % EJERCICIO 2 %
+
+% Hechos
+%padres e hijos
+padre(homer, pugsley).
+padre(homer, wednesday).
+madre(morticia, pugsley).
+madre(morticia, wednesday).
+% abuela
+madre(abuela_addams, tio_fester).
+madre(abuela_addams, tio_cosa).
+madre(abuela_addams, homer).
+%extra
+casado(homer, morticia).
+
+% Reglas
+casado(X, Y) :- casado(Y, X).
+hermano(X, Y) :- padre(Z, X), padre(Z, Y), X \= Y; madre(Z, X), madre(Z, Y), X \= Y.
+hijo(X, Y) :- padre(Y, X); madre(Y, X).
+abuela(X, Y) :- madre(Z, Y), madre(X, Z); padre(Z, Y), madre(X, Z).
+tio(X, Y) :- hermano(X, Z), padre(Z, Y); hermano(X, Z), madre(Z, Y).
+sobrino(X, Y) :- tio(Y, X).
+cunyado(X, Y) :- casado(Y, Z), hermano(X, Z). 
